a8e0932547eb0c7ffb8be94383425562f750f2e0
carlo_000 <carlo_000@cccarisco-PC>
2016-05-26 19:30:03 +0200

    Principal y Mi Menu hechas

--- temperatura/src/temperatura/Principal.java
+++ temperatura/src/temperatura/Principal.java
@@ -4,7 +4,67 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-
-	}
-
-}
+		Periodico per=null;
+		try
+		{
+			per=new Periodico();
+			System.out.println("Conexión con la base de datos> ¡OK!");
+		}
+		catch (Exception e)
+		{
+			System.err.println("Error: "e.getMessage());
+			//fin del programa
+			return;
+		}
+		
+		//Menú
+		MiMenu menu = new MiMenu();
+		menu.añadir("Listado");
+		menu.añadir("Nuevo");
+		menu.añadir("Modificar (uno a uno)");
+		menu.añadir("Eliminar (uno a uno)");
+		menu.añadir("Empieza por");
+		menu.añadir("Incluye");
+		menu.añadir("Medias");
+		menu.añadir("Salida");
+		
+		MiMenu menu reducido = new MiMenu();
+		reducido.añadir("Nuevo");
+		reducido.añadir("Salida");
+		
+		booblean salir = false;
+		do
+		{
+			if (per. estaVacia()==true)
+			{
+				Systme.out.println("BD vacía");
+				switch(reducido.ver("Periódico DAM"))
+				{
+				case 1:per.añadir();
+				break;
+				default:
+					salir=true;
+					break;
+				}
+			}
+			else
+			{
+				switch(menu.ver("Periodico DAM"))
+				{
+					case 1: per.listado();break;
+					case 2: per.añadir();break;
+					case 3: per.modificarUnoAUno();break;
+					case 4: per.eliminarUnoAUno();break;
+					case 5: per.empiezaPor();break;
+					case 6: per.incluye();break;
+					case 7: per.medias();break;
+					default:
+						salir=true;
+					break;
+				}
+			}
+		}
+		while (!salir);
+		System.out.println("CMA-DAM. Fin del programa");
+	}//Fin del main
+}//Fin de la clase
